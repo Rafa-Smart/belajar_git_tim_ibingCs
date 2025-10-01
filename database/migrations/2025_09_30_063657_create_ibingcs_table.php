adecb136077528829fba92672b178d518202fb10
FhadilPratama <fhadilpratama945@gmail.com>
2025-10-01 08:03:27 +0700

    menambahkan kolom1

--- database/migrations/2025_09_30_063657_create_ibingcs_table.php
+++ database/migrations/2025_09_30_063657_create_ibingcs_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('ibingcs', function (Blueprint $table) {
             $table->id();
             $table->string('nama');
+            $table->string('kelas');
             $table->timestamps();
         });
     }
